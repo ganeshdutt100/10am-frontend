5af11e955bcf11459dc41c00f9819ad260b508ca
Ganesh Dutt <gdutt2304@gmail.com>
2026-08-11 13:18:29 +0530

    js

---
+++ js/code1.js
@@ -0,0 +1,18 @@
+console.log("Hello, World!");
+
+document.writeln("This is a simple JavaScript code snippet.");
+// let , var , const
+var name = "Ganesh Dutt";
+console.log(name);
+var name = "Dutt";
+name = "Ganesh Dutt";
+console.log(name);
+
+let userName = "ganeshdutt@123";
+userName = "The_Ganesh_Dutt";
+// let userName = "ganeshdutt@123";
+console.log(userName);
+
+const userEmail = "trainerganeshdutt@gmail.com";
+// userEmail = "ganesh";
+console.log(userEmail);
