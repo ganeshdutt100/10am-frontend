36fc3e0b1aeb37c414a917da9588e970625a9065
Ganesh Dutt <gdutt2304@gmail.com>
2026-08-13 13:35:30 +0530

    Operators

---
+++ js/code3.js
@@ -0,0 +1,63 @@
+console.log("code3");
+
+// let a = 5;
+// let b = 2;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a * b);
+// console.log(a % 2);
+// a++;
+// console.log(a);
+// console.log(++a);
+// console.log(--a);
+// console.log(a--);
+
+// console.log(a);
+
+// let c = 2;
+// c += 5;
+// c -= 5;
+// c *= 5;
+// c /= 5;
+// c %= 5;
+
+// c = c + 5;
+// console.log(c);
+
+// let a = true;
+// let b = false;
+// let a = 7;
+// let b = "7";
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a != b);
+
+// let a = true;
+// let b = false;
+
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!a);
+
+// (and && , or ||, not!);
+//  true && false  = false
+//  true && true  = true
+//   false && false  = false
+
+//  true || false  = true
+//  true || true  = true
+//   false || false  = false
+
+// !true = false
+
+// let a = 4;
+// syntax
+// (condition)?true:false
+// let output = a % 2 === 0 ? "even" : "odd";
+// console.log(output);
