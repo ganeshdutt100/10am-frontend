76b820b171d638fd9a7a8bb0403c81b73324ed3e
Ganesh Dutt <gdutt2304@gmail.com>
2026-08-14 13:09:26 +0530

    if else

---
+++ js/code4.js
@@ -0,0 +1,26 @@
+console.log("code4");
+let a = 0;
+
+// if (condition) {
+// }
+
+// if (a >= 0) {
+//   console.log("positive");
+// }
+
+// if (a >= 0) {
+//   console.log("positive");
+// } else {
+//   console.log("Negative");
+// }
+
+// marks = 62;
+// if (marks >= 80) {
+//   console.log("A");
+// } else if (marks >= 60) {
+//   console.log("B");
+// } else if (marks >= 40) {
+//   console.log("C");
+// } else {
+//   console.log("Fail");
+// }
