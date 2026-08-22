f7cf59ee0f8522a0539e3495ab073b1b807f8e53
Ganesh Dutt <gdutt2304@gmail.com>
2026-08-22 13:50:09 +0530

    selector

---
+++ js/loop/code1.js
@@ -0,0 +1,32 @@
+// console.log("Loop");
+
+// for (let a = 1; a <= 10; a++) {
+//   console.log(a, "Ganesh Dutt");
+// }
+
+// let table = 34;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(table, "*", i, "=", i * table);
+// }
+// 2 *1  =2
+// 2 *2  =4
+// 2 *3  =6
+
+// let num = 5;
+
+// let fact = 1;
+
+// for (let i = 1; i <= num; i++) {
+//   fact = fact * i;
+// }
+// console.log(fact);
+
+let base = 5;
+let power = 3;
+let result = 1;
+
+for (let i = 1; i <= power; i++) {
+  result = result * base;
+}
+console.log(result);
