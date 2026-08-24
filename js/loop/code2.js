3fa1e165dc303eef1d0426e6a3eb44a375a77983
Ganesh Dutt <gdutt2304@gmail.com>
2026-08-24 12:14:44 +0530

    loops

---
+++ js/loop/code2.js
@@ -0,0 +1,24 @@
+// let a = 5;
+// while (a <= 10) {
+//   console.log(a);
+//   a++;
+// }
+
+// do while
+// let i = 10;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+// in  and of
+// let a = " Sumit ";
+//
+// let studentNames = ["Sumit", "Nandini", "Himanshi", "Angli"];
+//
+// for (let Sname in studentNames) {
+//   console.log(Sname);
+// }
+// for (let Sname of studentNames) {
+//   console.log(Sname);
+// }
