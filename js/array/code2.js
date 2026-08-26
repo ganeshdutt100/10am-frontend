17af99f352dd22030293fbadfb8048a82f5b0e61
Ganesh Dutt <gdutt2304@gmail.com>
2026-08-26 13:37:40 +0530

    method

---
+++ js/array/code2.js
@@ -0,0 +1,32 @@
+// let arr = new Array();
+// arr[0] = "Nandini";
+// arr[1] = "Arpit";
+// arr[2] = "Akash";
+// arr[3] = "Radha";
+// arr[4] = "Sumit";
+// arr[5] = "Himanshi";
+//
+// console.log(arr[0]);
+
+// let arr = new Array("Nandini", "sumit");
+// console.log(arr);
+
+// let arr = [1, 2, 3, 4, 5, 6];
+// let arr = [9, 22, 4449, 8, 11, 12, 1];
+
+// let output = arr.fill("js", 1, 4);
+// arr.push("js");
+// arr.shift();
+// arr.unshift("js");
+// arr.reverse();
+// arr.sort();
+
+// console.log(arr);
+
+// js,1,4  ,  1,js,js,js,5,6
+
+let arr = [1, 2, 34, 56, 7, 8, 90, 87];
+
+arr.splice(1, 3, "js", "html", "css", "tailwind", "react");
+
+console.log(arr);
