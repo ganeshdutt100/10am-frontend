05c553ef40c4d4fa5d332c1774a56fd9c664aad1
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-02 13:10:36 +0530

    Transform Methods

--- js/array/code3.js
+++ js/array/code3.js
@@ -1,4 +1,4 @@
-let arr1 = [12, 13, 14, 15, 16, 17, 18, , 16, 19, 20, 16];
+// let arr1 = [12, 13, 14, 15, 16, 17, 18, , 16, 19, 20, 16];
 // let arr2 = [2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // let output = arr1.concat(arr2);
@@ -11,3 +11,20 @@ let arr1 = [12, 13, 14, 15, 16, 17, 18, , 16, 19, 20, 16];
 // let output = arr1.toString();
 //
 // console.log(output);
+
+// let str = "Coding";
+
+// let output = Array.from(str);
+// console.log(output);
+
+// let num = [1, 2, 3, 4, 2, 5, 6, 4, 3, 7, 8, 9];
+// let output1 = new Set(num);
+// console.log(output1);
+//
+// let num = Array.from(new Set([1, 2, 3, 4, 2, 5, 6, 4, 3, 7, 8, 9]));
+// console.log(num);
+// let num = Array.from([1, 2, 3, 4], (num) => num * num);
+// console.log(num);
+
+// let value = Array.of(7, "js");
+// console.log(value);
