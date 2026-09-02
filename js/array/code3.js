7fe460058b17e3dc7344b0a1849667cc225afd58
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-02 12:55:32 +0530

    accessor methods

---
+++ js/array/code3.js
@@ -0,0 +1,13 @@
+let arr1 = [12, 13, 14, 15, 16, 17, 18, , 16, 19, 20, 16];
+// let arr2 = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let output = arr1.concat(arr2);
+// let output = arr1.includes(6);
+// let output = arr1.indexOf(16);
+// let output = arr1.join("  , ");
+// let output = arr1.lastIndexOf(16);
+// let output = arr1.indexOf(16, 5);
+// let output = arr1.slice(0, 5);
+// let output = arr1.toString();
+//
+// console.log(output);
