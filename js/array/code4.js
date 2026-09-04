36cde09d453551a127eafe66dacd3b0afdc91761
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-04 13:15:02 +0530

    code4

---
+++ js/array/code4.js
@@ -0,0 +1,17 @@
+// const arr1 = [1, 2, 36, 8, 4, 56, 7];
+// const arr2 = [11, 12, 136, 18, 14, 516, 17];
+// console.log([...arr1, ...arr2, "express"]);
+
+// const user = {
+//   name: "Ganesh Dutt",
+//   course: "FS",
+//   phone: 9876567890,
+// };
+// console.log({ ...user, course: "MERN" });
+// console.log(arr1.concat(arr2), "express");
+
+const arr = [12, 45, 67, 8, 654, 32, 45];
+const output1 = Math.max(...arr);
+const output2 = Math.min(...arr);
+
+console.log(output1);
