b32498ea59e11a9092fc395834006968466f90ef
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-08 13:49:00 +0530

    string

--- js/String/code1.js
+++ js/String/code1.js
@@ -7,9 +7,9 @@
 //
 // let output = `Hello : ${studentName} `;
 // console.log(output.length);
-
-let value1 = "Radha";
-let value2 = "world";
+//
+// let value1 = "Radha";
+// let value2 = "world";
 // let output = value.substring(0, 5);
 // let output = value.substr(0, 5);
 // let output = value.split(",");
@@ -20,6 +20,13 @@ let value2 = "world";
 // let output = value1.trim().length;
 // let output = value1.indexOf("h");
 // let output = value1.replace("a", "o");
-let output = value1.replace(/a/g, "o");
+// let output = value1.replace(/a/g, "o");
+//
+// let otuput = console.log(output);
 
-let otuput = console.log(output);
+let a = "hello world";
+let output = a
+  .split(" ")
+  .map((x) => x[0].toUpperCase() + x.slice(1))
+  .join(" ");
+console.log(output);
